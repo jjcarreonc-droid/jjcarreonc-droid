8a0bb40a5fa0e0781a4cd25b2e69cc3be05b7279
jjcarreonc-droid <280094489+jjcarreonc-droid@users.noreply.github.com>
2026-04-28 18:27:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new  primera vez app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+import pandas as pd
+
+import numpy as np
+
+st.title("📊 Gráfica interactiva")
+
+# Slider para controlar datos
+
+n = st.slider("Cantidad de datos", 10, 100, 50)
+
+data = pd.DataFrame(
+
+    np.random.randn(n, 2),
+
+    columns=["Ventas", "Costos"]
+
 )
+
+st.line_chart(data)
